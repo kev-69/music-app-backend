ffbbdf79d6b133f7c7dbaa92d5196370d9536f94
kev-69 <bismarkobuobi19@gmail.com>
2025-01-11 20:32:13 +0000

    commented a line of code for the mongodb connect out because it was deprecated on node

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -3,13 +3,13 @@ const mongoose = require('mongoose');
 const DBConnection = async () => {
     try {
         await mongoose.connect(process.env.DB_HOST, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
+            // useNewUrlParser: true,
+            // useUnifiedTopology: true,
             // useCreateIndex: true,
         });
-        console.log('DB connected');
+        console.log('Database connected');
     } catch (error) {
-        console.log('DB connection failed', error);
+        console.log('Database connection failed', error);
     }
 }
 
